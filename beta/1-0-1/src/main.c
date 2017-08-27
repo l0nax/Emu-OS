2bf7247722f39671596ebfd4601243e11458b8e8
Unknown <benniciemanuel78@gmail.com>
2017-08-27 14:55:17 +0200

    Start File for OS.

---
+++ beta/1-0-1/src/main.c
@@ -0,0 +1,15 @@
+
+void init(void) {
+
+        const char hw[] = "Hello, World!";
+        int i;
+        char *video = (char*) 0xb8000;
+
+        for(i=0; hw[i] != '\0'; i++) {
+                // Zeichen 'i' in den Videospeicher kopieren
+                video[i*2] = hw[i];
+
+                // 0x07 = Hellgrau auf Schwarz
+                video[i*2+1] = 0x07;
+        }
+}

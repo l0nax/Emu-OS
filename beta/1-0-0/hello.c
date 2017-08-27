bc0f2480fa91479c5f5fae209558fb1413a46ab0
root <root@localhost.localdomain>
2017-08-27 14:09:24 +0200

    Init All

---
+++ beta/1-0-0/hello.c
@@ -0,0 +1,13 @@
+#define SYSCALL_EXIT 1
+#define SYSCALL_WRITE 4
+
+char hello[] = "Hello, World!\n";
+
+void _start(void) {
+        // sys_write(1, hello, sizeof(hello))
+        asm("int $0x80" : : "a" (SYSCALL_WRITE), "b" (1), "c" (hello), "d"
+                (sizeof(hello)));
+        // sys_exit(0)
+        asm("int $0x80" : : "a" (SYSCALL_EXIT), "b" (0));
+}
+// This is a Commit
